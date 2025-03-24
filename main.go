4571dc6188a399a68cc69f31a065b6178abb84a6
Ice-i-Cream <donghao191@qq.com>
2025-03-24 11:39:56 +0800

    Changes

---
+++ main.go
@@ -0,0 +1,14 @@
+// main.go
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, Gin!")
+	})
+	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
+}
